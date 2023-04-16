086aa05e4ec31152206c2fab080bfce1836eb3a9
shiviravisankar <shiviravis@gmail.com>
2023-04-15 22:48:10 -0400

    updated script.js

--- assets/script.js
+++ assets/script.js
@@ -100,20 +100,21 @@ function displayCocktail(cocktail){
     }
 }
 
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+    // NAV MENU DROPDOWN
+    function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
       }
     }
-  }
-}
 
