ac4c5957bef8529ec033e326afeb67a683dddf66
shiviravisankar <shiviravis@gmail.com>
2023-04-15 22:27:31 -0400

    top nav added to page

--- assets/script.js
+++ assets/script.js
@@ -99,5 +99,21 @@ function displayCocktail(cocktail){
         svgElements[i].style.fill = color;
     }
 }
-  
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
